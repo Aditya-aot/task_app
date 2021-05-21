6db11e664579b0fa419c6a4c865f20fde2cd666d
aditya-aot <aditya.aot.11@gmail.com>
2021-05-21 22:43:45 +0530

    part2

--- task/urls.py
+++ task/urls.py
@@ -3,6 +3,8 @@ from . import views
 from task.views import home
 
 urlpatterns = [
-    path('', views.home , name='home') 
-
+    path('', views.home , name='home') ,
+    path('task/', views.home , name='home') ,
+    path('add/', views.home , name='home') ,
+    path("delete/<int:task_id>/", views.delete_view , name='delete_view')
 ]
--- task/views.py
+++ task/views.py
@@ -2,11 +2,20 @@
 from django.shortcuts import render
 from .models import task_model
 from .forms import TaskForm
+from django.http import HttpResponseRedirect
+
+
 # Create your views here.
 
 def home(request):
     form = TaskForm()
     task_data = task_model.objects.all()
+
+    if request.method == 'POST' :
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+
 
     context = { 'form':form ,
                 'task_data' : task_data
@@ -16,3 +25,7 @@ def home(request):
 
     return render(request, 'home.html', context)
 
+def delete_view(request , task_id) :
+    task_to_delete = task_model.objects.get(id=task_id)
+    task_to_delete.delete()
+    return HttpResponseRedirect('/task/') 
